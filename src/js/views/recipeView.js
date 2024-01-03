7a06d29a43effdfda45122d288eeb68e21722ea9
chloelam123 <chloeying916@icloud.com>
2024-01-02 20:01:54 -0500

    add nicer error handling with default message

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -4,6 +4,7 @@ import { Fraction } from 'fractional';
 class RecipeView {
   #parentElement = document.querySelector('.recipe');
   #data;
+  #errorMessage = 'We could not find that recipe. Please try another one!';
 
   render(data) {
     this.#data = data;
@@ -30,7 +31,8 @@ class RecipeView {
     this.#parentElement.insertAdjacentHTML('afterbegin', markup);
   }
 
-  renderError(message) {
+  //set default error message
+  renderError(message = this.#errorMessage) {
     const markup = `
         <div class="error">
             <div>
